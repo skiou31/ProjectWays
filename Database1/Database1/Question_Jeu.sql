8c4dd1dd9f34c2b201aac4780975e0fe454359b5
Gregory Le Gall <legall.gregory@gmail.com>
2018-04-18 20:21:40 +0200

    add project database

---
+++ Database1/Database1/Question_Jeu.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Question_Jeu]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Intitule] VARCHAR(50) NULL, 
+    [Reponse] VARCHAR(50) NULL
+)
---
+++ Database1/Database1/Question_Orientation.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Question_Orientation]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Intitule] VARCHAR(50) NULL, 
+    [Reponse] VARCHAR(50) NULL
+)
